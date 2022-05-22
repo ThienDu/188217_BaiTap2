eb3a8d638de0fe92cc7819e487d525df39086f5b
ThienDu <thiendu2507@gmail.com>
2022-05-22 14:38:59 +0700

    add bai-tap2

---
+++ 188217_BaiTap2/188217_BaiTap2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _188217_BaiTap2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
